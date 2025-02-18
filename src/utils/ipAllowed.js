5f3a33a5a46e72a308c60aa0ea77f52f79e69df3
Anders Friborg <frippera@gmail.com>
2025-02-18 08:17:21 +0100

    Fixed dev mode

--- src/utils/ipAllowed.js
+++ src/utils/ipAllowed.js
@@ -1,15 +1,26 @@
 const ipAllowed = ({ headers, response }) => {
-  const ip = headers['x-real-ip'];
+  const ip = headers["x-real-ip"];
   const ipFilter = process.env.IP_ADDRESS_ALLOWED;
 
-  if (!ip || !ipFilter) {
-    console.log("### ip or filter missing: ", ip);
-    return response.status(400).send({ response: { status: 400, statusText: "Missing ip or ip-filter" } });
-  } else {
-    console.log("### ip filter run for: ", ip);
+  if (!ipFilter) {
+    console.log("### ip-filter missing, please configure env");
+    return response.status(400).send({
+      response: { status: 400, statusText: "Missing ip-filter" },
+    });
+  }
+
+  if (!ipFilter.includes("127.0.0.1")) {
+    if (!ip) {
+      console.log("### ip missing: ", ip);
+      return response.status(400).send({
+        response: { status: 400, statusText: "Missing ip" },
+      });
+    } else {
+      console.log("### ip filter run for: ", ip);
+    }
   }
 
   return ipFilter.includes(ip);
-}
+};
 
 export default ipAllowed;
